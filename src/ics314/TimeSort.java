2e7b6e24eac6c592bec29cf7f312bb1a6ea2951e
adeadant <nigelng@hawaii.edu>
2016-04-24 18:19:07 -1000

    Added if check if no files were entered

--- src/ics314/TimeSort.java
+++ src/ics314/TimeSort.java
@@ -12,61 +12,67 @@ import java.util.Scanner;
 public class TimeSort {
 
 	/**
-	 * startsort sorts the given files by their date and time then passes the data to addcircledist
+	 * startsort sorts the given files by their date and time then passes the
+	 * data to addcircledist
+	 * 
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void startsort(ArrayList<String> args) throws IOException {
+		if (args.size() > 0) {
+			String[] names = new String[args.size()];
+			for (int i = 0; i < args.size(); i++) {
+				names[i] = args.get(i);
+			}
+			int[] times = new int[args.size()];
+			ArrayList<Integer> dates = new ArrayList<Integer>();
+			for (int i = 0; i < args.size(); i++) {
+				times[i] = findstarttime(args.get(i));
+				dates.add(findstartdate(args.get(i)));
+			}
 
-		String[] names = new String[args.size()];
-		for (int i = 0; i < args.size(); i++) {
-			names[i] = args.get(i);
-		}
-		int[] times = new int[args.size()];
-		ArrayList<Integer> dates = new ArrayList<Integer>();
-		for (int i = 0; i < args.size(); i++) {
-			times[i] = findstarttime(args.get(i));
-			dates.add(findstartdate(args.get(i)));
-		}
+			List<List<Integer>> manydates = new ArrayList<List<Integer>>();
+			List<List<String>> manyfiles = new ArrayList<List<String>>();
+			HashSet<Integer> unique = new HashSet<Integer>(dates);
+			for (Integer stuff : unique) {
+				System.out.println(stuff);
+			}
+			Object[] elements = unique.toArray();
+			for (int i = 0; i < unique.size(); i++) {
+				manydates.add(new ArrayList<Integer>());
+				manyfiles.add(new ArrayList<String>());
+			}
 
-		List<List<Integer>> manydates = new ArrayList<List<Integer>>();
-		List<List<String>> manyfiles = new ArrayList<List<String>>();
-		HashSet<Integer> unique = new HashSet<Integer>(dates);
-		for(Integer stuff: unique){
-			System.out.println(stuff);
-		}
-		Object[] elements = unique.toArray();
-		for (int i = 0; i < unique.size(); i++) {
-			manydates.add(new ArrayList<Integer>());
-			manyfiles.add(new ArrayList<String>());
-		}
-
-		for (int i = 0; i < dates.size(); i++) {
-			for (int j = 0; j < elements.length; j++) {
-				if (elements[j].equals(dates.get(i))) {
-					manydates.get(j).add(times[i]);
-					manyfiles.get(j).add(names[i]);
+			for (int i = 0; i < dates.size(); i++) {
+				for (int j = 0; j < elements.length; j++) {
+					if (elements[j].equals(dates.get(i))) {
+						manydates.get(j).add(times[i]);
+						manyfiles.get(j).add(names[i]);
+					}
 				}
 			}
-		}
-		for (int i = 0; i < manydates.size(); i++) {
-			caltime[] cals = new caltime[manydates.get(i).size()];
-			for (int j = 0; j < manydates.get(i).size(); j++) {
-				cals[j] = new caltime(manydates.get(i).get(j), manyfiles.get(i).get(j));
-			}
-		Arrays.sort(cals, new caltimeComp());
-		
-		for (int k = 0; k < cals.length; k++) {
-			System.out.println(cals[k].getTime() + " " + cals[k].getFilename());
-		}
-		System.out.println();
+			for (int i = 0; i < manydates.size(); i++) {
+				caltime[] cals = new caltime[manydates.get(i).size()];
+				for (int j = 0; j < manydates.get(i).size(); j++) {
+					cals[j] = new caltime(manydates.get(i).get(j), manyfiles
+							.get(i).get(j));
+				}
+				Arrays.sort(cals, new caltimeComp());
 
-		addcircledist(cals);
+				for (int k = 0; k < cals.length; k++) {
+					System.out.println(cals[k].getTime() + " "
+							+ cals[k].getFilename());
+				}
+				System.out.println();
+
+				addcircledist(cals);
+			}
 		}
 	}
 
 	/**
 	 * findstartdate finds the starting date in the .ics file
+	 * 
 	 * @param filename
 	 * @return
 	 */
@@ -95,6 +101,7 @@ public class TimeSort {
 
 	/**
 	 * findstarttime finds the starting time in the .ics file
+	 * 
 	 * @param filename
 	 * @return
 	 */
@@ -122,7 +129,9 @@ public class TimeSort {
 	}
 
 	/**
-	 * addcircledist calls GreatCircleDist methods to compute the great circle distance and input it into the files. 
+	 * addcircledist calls GreatCircleDist methods to compute the great circle
+	 * distance and input it into the files.
+	 * 
 	 * @param cals
 	 * @throws IOException
 	 */

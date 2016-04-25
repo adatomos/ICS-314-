0a38c99efb13fcd54ad914b490c513634dfcd01a
adeadant <nigelng@hawaii.edu>
2016-04-24 17:55:17 -1000

    Added date checking

--- src/ics314/TimeSort.java
+++ src/ics314/TimeSort.java
@@ -5,39 +5,96 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class TimeSort {
 
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void startsort(ArrayList<String> args) throws IOException {
-		/**if (args.size() < 2) {
-			System.out
-					.println("Need more than 1 .ics file for Great Circle Distance");
-		} else {*/
-			String[] names = new String[args.size()];
-			for (int i = 0; i < args.size(); i++) {
-				names[i] = args.get(i);
-			}
-			int[] times = new int[args.size()];
-			for (int i = 0; i < args.size(); i++) {
-				times[i] = findstarttime(args.get(i));
-			}
-			caltime[] cals = new caltime[args.size()];
-			for (int i = 0; i < names.length; i++) {
-				cals[i] = new caltime(times[i], names[i]);
-			}
 
-			Arrays.sort(cals, new caltimeComp());
+		String[] names = new String[args.size()];
+		for (int i = 0; i < args.size(); i++) {
+			names[i] = args.get(i);
+		}
+		int[] times = new int[args.size()];
+		ArrayList<Integer> dates = new ArrayList<Integer>();
+		for (int i = 0; i < args.size(); i++) {
+			times[i] = findstarttime(args.get(i));
+			dates.add(findstartdate(args.get(i)));
+		}
 
-			for (int i = 0; i < names.length; i++) {
-				System.out.println(cals[i].getTime() + " "
-						+ cals[i].getFilename());
+		List<List<Integer>> manydates = new ArrayList<List<Integer>>();
+		List<List<String>> manyfiles = new ArrayList<List<String>>();
+		HashSet<Integer> unique = new HashSet<Integer>(dates);
+		for(Integer stuff: unique){
+			System.out.println(stuff);
+		}
+		Object[] elements = unique.toArray();
+		for (int i = 0; i < unique.size(); i++) {
+			manydates.add(new ArrayList<Integer>());
+			manyfiles.add(new ArrayList<String>());
+		}
+
+		for (int i = 0; i < dates.size(); i++) {
+			for (int j = 0; j < elements.length; j++) {
+				if (elements[j].equals(dates.get(i))) {
+					manydates.get(j).add(times[i]);
+					manyfiles.get(j).add(names[i]);
+				}
 			}
+		}
+		for (int i = 0; i < manydates.size(); i++) {
+			caltime[] cals = new caltime[manydates.get(i).size()];
+			for (int j = 0; j < manydates.get(i).size(); j++) {
+				cals[j] = new caltime(manydates.get(i).get(j), manyfiles.get(i).get(j));
+			}
+		Arrays.sort(cals, new caltimeComp());
+		
+		for (int k = 0; k < cals.length; k++) {
+			System.out.println(cals[k].getTime() + " " + cals[k].getFilename());
+		}
+		System.out.println();
 
-			addcircledist(cals);
-		//}
+		addcircledist(cals);
+		}
 	}
 
+	/**
+	 * @param filename
+	 * @return
+	 */
+	private static int findstartdate(String filename) {
+		String str = "";
+		int date = 0;
+		File file = new File(filename);
+		try {
+			Scanner in = new Scanner(file);
+			while (in.hasNext()) {
+				String line = in.nextLine();
+				if (line.contains("DTSTART")) {
+					str = line.substring(line.lastIndexOf(':') + 1,
+							line.lastIndexOf('T'));
+					date = Integer.parseInt(str);
+					// System.out.println(time);
+				}
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return date;
+	}
+
+	/**
+	 * @param filename
+	 * @return
+	 */
 	private static int findstarttime(String filename) {
 		String str = "";
 		int time = 0;
@@ -57,40 +114,43 @@ public class TimeSort {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		return time;
 	}
 
-	public static void addcircledist(caltime[] cals) throws IOException {
-                float[] deg1, deg2, dist;
-                ArrayList<String> gotGEO = new ArrayList();
-                ArrayList<String> noGEO = new ArrayList();
-                for (int i = 0; i < cals.length; i++){
-                    if (GreatCircleDistance.IfGotGeo(cals[i].getFilename())){
-                        gotGEO.add(cals[i].getFilename());
-                    }
-                    else noGEO.add(cals[i].getFilename());
-                
-                }
-                if(gotGEO.size()>1){
-		for (int i = 0,j = 1; j < gotGEO.size(); i++,j++) {
+	/**
+	 * @param cals
+	 * @throws IOException
+	 */
+	private static void addcircledist(caltime[] cals) throws IOException {
+		float[] deg1, deg2, dist;
+		ArrayList<String> gotGEO = new ArrayList();
+		ArrayList<String> noGEO = new ArrayList();
+		for (int i = 0; i < cals.length; i++) {
+			if (GreatCircleDistance.IfGotGeo(cals[i].getFilename())) {
+				gotGEO.add(cals[i].getFilename());
+			} else
+				noGEO.add(cals[i].getFilename());
+
+		}
+		if (gotGEO.size() > 1) {
+			for (int i = 0, j = 1; j < gotGEO.size(); i++, j++) {
 				deg1 = GreatCircleDistance.GetGeo(gotGEO.get(i));
 				deg2 = GreatCircleDistance.GetGeo(gotGEO.get(j));
 				dist = GreatCircleDistance.GreatCircleDist(deg1[0], deg1[1],
 						deg2[0], deg2[1]);
-				GreatCircleDistance.AddComment(gotGEO.get(i), dist[0],
-						dist[1]);
+				GreatCircleDistance.AddComment(gotGEO.get(i), dist[0], dist[1]);
 				if (j == (gotGEO.size() - 1)) {
 					GreatCircleDistance.AddCommentLastEvent(gotGEO.get(j));
 				}
-                }
-                }else if(gotGEO.size()==1) {
-                    GreatCircleDistance.AddCommentOnlyEventWithLocation(gotGEO.get(0));
-                }
-                if(noGEO.size()>0){
-                    for(int k = 0; k < noGEO.size();k++ ){
-                       GreatCircleDistance.AddCommentEventNoLocation(noGEO.get(k)); 
-                    }
-                }
+			}
+		} else if (gotGEO.size() == 1) {
+			GreatCircleDistance.AddCommentOnlyEventWithLocation(gotGEO.get(0));
+		}
+		if (noGEO.size() > 0) {
+			for (int k = 0; k < noGEO.size(); k++) {
+				GreatCircleDistance.AddCommentEventNoLocation(noGEO.get(k));
+			}
+		}
 	}
 }
